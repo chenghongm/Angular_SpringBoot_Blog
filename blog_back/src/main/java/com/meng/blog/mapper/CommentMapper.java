9954c3ce338c8bc1bf588b2bfcaade974bcbe011
chenghongm <mch8418@gmail.com>
2021-05-01 11:21:10 -0400

    implement mappers

---
+++ blog_back/src/main/java/com/meng/blog/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.meng.blog.mapper;
+
+import org.mapstruct.*;
+
+import com.meng.blog.dto.CommentsDto;
+import com.meng.blog.model.Comment;
+import com.meng.blog.model.Post;
+import com.meng.blog.model.User;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    @Mapping(target = "user", source = "user")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
---
+++ blog_back/src/main/java/com/meng/blog/mapper/SubredditMapper.java
@@ -0,0 +1,24 @@
+package com.meng.blog.mapper;
+
+import java.util.List;
+
+import org.mapstruct.*;
+
+import com.meng.blog.dto.SubredditDto;
+import com.meng.blog.model.Post;
+import com.meng.blog.model.Subreddit;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
